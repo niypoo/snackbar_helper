e8b2fcd688a5da892593b9645371bca498c6d55b
Mahmoud Nabhan <niypoo@gmail.com>
2023-09-13 11:56:38 +0300

    add error , success functions

--- lib/snackbar.service.dart
+++ lib/snackbar.service.dart
@@ -3,10 +3,11 @@ import 'package:get/get.dart';
 
 class SnackbarHelper {
   static void show({
+    required String title,
     String? body,
-    String? title,
     IconData? icon,
     Color? color,
+    Color? textColor,
     TextButton? action,
     int duration = 4,
     SnackPosition position = SnackPosition.TOP,
@@ -17,19 +18,49 @@ class SnackbarHelper {
 
     // Show
     Get.rawSnackbar(
-        title: title,
         message: body,
         backgroundColor: color ?? Get.theme.primaryColor,
         mainButton: action,
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        titleText: Text(
+          title,
+          style: Get.textTheme.titleLarge!.copyWith(
+            color: textColor ?? Get.theme.secondaryHeaderColor,
+          ),
+        ),
+        messageText: Text(
+          body ?? '',
+          style: Get.textTheme.bodyLarge!.copyWith(
+          ),
+        ),
         icon: icon != null
             ? Icon(
                 icon,
-                color: Get.theme.colorScheme.secondary,
+                color: textColor ?? Get.theme.secondaryHeaderColor,
               )
             : null,
         snackPosition: position,
         snackStyle: style,
         duration: Duration(seconds: duration));
   }
+
+  static void error({
+    required String title,
+    String? body,
+  }) =>
+      show(
+        color: Colors.red[900],
+        title: title,
+        body: body,
+      );
+
+  static void success({
+    required String title,
+    String? body,
+  }) =>
+      show(
+        color: Colors.green[900],
+        title: title,
+        body: body,
+      );
 }
